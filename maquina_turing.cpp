3258193581a4ef15ccb3d45fdb4e1ff6dd1934ef
pamelapalomo <pamela.palomomrt@gmail.com>
2017-01-30 19:19:39 -0600

    Update maquina_turing.cpp

--- maquina_turing.cpp
+++ maquina_turing.cpp
@@ -11,14 +11,14 @@ using namespace std;
 int main(int argc, char** argv) {
 	
 	/*Recibe un archivo de texto cuya primer linea tiene el contenido original de la cinta
-	y cada renglon restante contiene la definicion de la funcion de transicion, hacienod uso 
+	y cada renglon restante contiene la definicion de la funcion de transicion, haciendo uso 
 	de cinco elementos: e s e' s' d
 	Siendo e: estado
 		s: sìmbolo
 		e' el estado resultante
 		s' el simbolo resultante
 		d: la direccion en la cual se mueve el apuntador (d,i,n)
-		En este repositorio se tienen tres ejemplos dearchivos de entrada:
+		En este repositorio se tienen tres ejemplos de archivos de entrada:
 		entrada_es_par.txt, entrada_multiplicarx2.txt y entrada_sumar1.txt
 		*/
 	Maquina_Turing _maquina(argv);
